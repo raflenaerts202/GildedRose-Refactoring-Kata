3507418a02004b8e8a3fcaed660046a195ec74b5
Raf Lenaerts <raf.lenaerts@persgroep.be>
2023-11-23 22:28:41 +0100

    Grouped by item name

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,59 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+        for (Item item : items) {
+            updateItem(item);
         }
+    }
+
+    private static void updateItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                if (item.quality < 50) {
+                    increaseQuality(item);
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.quality < 50 && item.sellIn < 0) {
+                    increaseQuality(item);
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    increaseQuality(item);
+
+                    if (item.sellIn < 11) {
+                        increaseQuality(item);
+                    }
+
+                    if (item.sellIn < 6) {
+                        increaseQuality(item);
+                    }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if (item.quality > 0) {
+                    decreaseQuality(item);
+                }
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0 && item.quality > 0) {
+                    decreaseQuality(item);
+                }
+                break;
+        }
+    }
+
+    private static void decreaseQuality(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    private static void increaseQuality(Item item) {
+        item.quality = item.quality + 1;
     }
 }
